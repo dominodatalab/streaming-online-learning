fcd58433b71911671b8770d828e6855898eb263b
wadkars <wadkar.sameer@gmail.com>
2021-02-04 10:50:07 -0500

    Update with instructions

--- src/multi_threaded_kafka_consumer.py
+++ src/multi_threaded_kafka_consumer.py
@@ -20,6 +20,7 @@ class ModelInference(object):
         :type interval: int
         :param interval: Check interval, in seconds
         """
+        self.model = None;#Create the model instance here
         self.my_id = my_id
         self.t = request_topic
         self.result_t = inference_topic
@@ -65,12 +66,12 @@ class ModelInference(object):
                 ingest_ts = message['ingestTs']
                 my_id = message['time']
                 truth = message['Class']
-                y_hat = truth
+                y_hat = truth ## Replace with model.predict & model.learn_one(
                 inference_ts = time.time()
                 out = {}
                 out['ingest_ts'] = ingest_ts
                 out['my_id'] = my_id
-                out['truth'] = truth
+                out['truth'] = truth ## model.learn_one(Y,Y_HAT)
                 out['y_hat'] = y_hat
                 out['inference_ts'] = inference_ts
 
@@ -78,7 +79,7 @@ class ModelInference(object):
                 i = i + 1
                 partition = i % self.result_t_p
                 #print('sending to ' + self.result_t + ' ' + str(self.result_t_p) + ' ' + str(out))
-                self.producer.send(self.result_t, value=out)
+                self.producer.send(self.result_t, value=out,partition=partition)
                 #self.producer.flush()
                 if (i % 1000 == 0):
                     self.producer.flush()
@@ -105,9 +106,51 @@ if __name__ == '__main__':
     example6 = ModelInference(my_id=6, bootstrap_servers=bootstrap_servers, list_of_partitions=[6],request_topic=request_topic,inference_topic=inference_topic,group_id='my_grp-1')
     example7 = ModelInference(my_id=7, bootstrap_servers=bootstrap_servers, list_of_partitions=[7],request_topic=request_topic,inference_topic=inference_topic,group_id='my_grp-1')
 
+
+    #example0 = ModelInference(my_id=0, bootstrap_servers=bootstrap_servers, list_of_partitions=[0,1],request_topic=request_topic,inference_topic=inference_topic,group_id='my_grp-1')
+    #example1 = ModelInference(my_id=1, bootstrap_servers=bootstrap_servers, list_of_partitions=[2,3],request_topic=request_topic,inference_topic=inference_topic,group_id='my_grp-1')
+    #example2 = ModelInference(my_id=2, bootstrap_servers=bootstrap_servers, list_of_partitions=[4,5],request_topic=request_topic,inference_topic=inference_topic,group_id='my_grp-1')
+    #example3 = ModelInference(my_id=3, bootstrap_servers=bootstrap_servers, list_of_partitions=[6,7],request_topic=request_topic,inference_topic=inference_topic,group_id='my_grp-1')
     time.sleep(3)
     print('Checkpoint')
     time.sleep(100000)
     print('Bye')
 
 
+'''
+            data['time'] = rows['Time']
+            data['my_id'] = i
+            data['ingestTs'] = time.time()
+            data['V1'] = rows['V1']
+            data['V2'] = rows['V2']
+            data['V3'] = rows['V3']
+            data['V4'] = rows['V4']
+            data['V5'] = rows['V5']
+            data['V6'] = rows['V6']
+            data['V7'] = rows['V7']
+            data['V8'] = rows['V8']
+            data['V9'] = rows['V9']
+            data['V10'] = rows['V10']
+            data['V11'] = rows['V11']
+            data['V12'] = rows['V12']
+            data['V13'] = rows['V13']
+            data['V14'] = rows['V14']
+            data['V15'] = rows['V15']
+            data['V16'] = rows['V16']
+            data['V17'] = rows['V17']
+            data['V18'] = rows['V18']
+            data['V19'] = rows['V19']
+            data['V20'] = rows['V20']
+            data['V21'] = rows['V21']
+            data['V22'] = rows['V22']
+            data['V23'] = rows['V23']
+            data['V24'] = rows['V24']
+            data['V25'] = rows['V25']
+            data['V26'] = rows['V26']
+            data['V27'] = rows['V27']
+            data['V28'] = rows['V28']
+            data['V28'] = rows['V28']
+            data['Amount'] = rows['Amount']
+            data['Class'] = rows['Class']
+
+'''
