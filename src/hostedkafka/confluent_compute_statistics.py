b7a4595d9d08f2c5c30e8eb7fc724552f354baa6
wadkars <wadkar.sameer@gmail.com>
2021-02-06 18:40:54 -0500

    Update the compute statistics

--- src/hostedkafka/confluent_compute_statistics.py
+++ src/hostedkafka/confluent_compute_statistics.py
@@ -7,17 +7,17 @@ from confluent_kafka.cimpl import KafkaError, KafkaException
 from confluent_kafka import TopicPartition
 import datetime
 import statistics
-user = '<ACCESS_KEY>'
-pwd = '<SECRET_KEY>'
-bsts = '<BOOTSTRAP_SERVER>'
-t = '<TOPIC_NAME>'
+user = 'SHHNBVCDDYYMBJ6Z'
+pwd = 'gcYXYis4Z0wN8s3jcLONr3xnisiwmIGIvRKUx7D+5FCw5E17of2+4+pitxFoneMq'
+bsts = 'pkc-4kgmg.us-west-2.aws.confluent.cloud:9092'
+t = <TOPIC_NAME>
 conf = {'bootstrap.servers': bsts,
         'sasl.mechanism': 'PLAIN',
         'security.protocol': 'SASL_SSL',
         'sasl.username': user,
         'sasl.password': pwd,
         'ssl.ca.location': '/tmp/cacert.pem',
-        'group.id': 'x-2',
+        'group.id': <PROVIDE_A_UNIQUE_VALUE_FOR_EACH_RUN>,
         'auto.offset.reset': 'smallest'}
 running = True
 
@@ -28,9 +28,10 @@ def basic_consume_loop(consumer, topics):
         i=0
         message = {}
         while running:
-            i = i + 1
+
             msg = consumer.poll(timeout=1.0)
             if msg is None: continue
+
             message = {}
             if msg.error():
                 if msg.error().code() == KafkaError._PARTITION_EOF:
@@ -42,13 +43,15 @@ def basic_consume_loop(consumer, topics):
             else:
 
                 message = loads(msg.value().decode("utf-8"))
-                print(message)
-                durs.append(durs.append(message['dur_evt_inf']))
+                #print(message)
+                if not message['dur_evt_inf'] is None:
+                    i = i + 1
+                    durs.append(message['dur_evt_inf'])
             if(i==1000000):
                 break
             #durs.append(m['dur_evt_inf'])
-            i = i + 1
-            if (i % 10000 == 0):
+
+            if (i % 1000 == 0):
                 print(message)
                 #now2 = datetime.now()
                 print(i)
@@ -56,15 +59,17 @@ def basic_consume_loop(consumer, topics):
     finally:
         # Close down consumer to commit final offsets.
         consumer.close()
-    mean = statistics.mean(durs)
-    median = statistics.median(durs)
-    max = max(durs)
-    min = min(durs)
+        #print(durs)
+        mean = statistics.mean(durs)
+        median = statistics.median(durs)
+        max1 = max(durs)
+        min2 = min(durs)
 
-    print('max=' + str(max))
-    print('min=' + str(min))
-    print('avg=' + str(mean))
-    print('med=' + str(median))
+        print('max=' + str(max1))
+        print('min=' + str(min2))
+        print('avg=' + str(mean))
+        print('med=' + str(median))
+        print('total obs =' + str(len(durs)))
 def shutdown():
     running = False
 consumer = Consumer(conf)
